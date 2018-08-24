c86d99c26320bf1e0e6ca7dfece0b22dfbc08624
Matt Cooper <mattc@xbox.com>
2018-08-24 13:38:05 -0400

    ??

--- .vsts/start_mysql.sh
+++ .vsts/start_mysql.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-export MYSQL_ROOT_PASSWORD=super-secret
-sudo -u mysql /usr/local/bin/docker-entrypoint.sh mysqld
+sudo -u mysql export MYSQL_ROOT_PASSWORD=super-secret && /usr/local/bin/docker-entrypoint.sh mysqld
 sleep 10
 mysql --host=127.0.0.1 --port=3306 -u root --password=secret --execute="SELECT User, Host FROM mysql.user"
